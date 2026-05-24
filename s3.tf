630b9a0827ab89d437fdc1ed8690d360f5c1df53
abhiram2627 <abhiram.kulakarni@outlook.com>
2026-05-24 15:22:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafa77.flm77.moonobucket"
+  bucket = "abhiram2026.devops.monolethic.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
